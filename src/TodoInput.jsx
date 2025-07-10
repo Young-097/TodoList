fa9a4da9436cfb1f3cb8aeb40b87021661eb74f9
Yong-097 <vosejgo@gmail.com>
2025-07-10 16:27:23 +0900

    feat:버튼기능구현

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-function InputValue () {
+function TodoInput () {
+    const [todoList, setTodoList] = useState([])
     const [inputValue, setInputValue] = useState()
+
+    useEffect(()=>{
+        
+    },[inputValue])
     return(
         <>
             <input type="text" value={inputValue} onChange={(event)=>{setInputValue(event.target.value)}}/>
+            <button onClick={()=>{
+                const newTodo = {id: todoList.length, todo: inputValue, isComplete: false}
+                const Arr = [...todoList,newTodo];
+                setTodoList(Arr);
+                setInputValue("");
+            }}>등록</button>
         </>
     )
 }
+
+export default TodoInput;
