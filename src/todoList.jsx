cc6bbe31ef2adfb246f95f59285b167aec7d038f
Yong-097 <vosejgo@gmail.com>
2025-07-11 13:43:17 +0900

    6단계 완료

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
 
 function TodoList ({todoList, setTodoList}) {
+    const [editingId, setEditingId] = useState(null);
+    const [editInput, seteditInput] = useState('')
 
     const toggleCheckbox = (index)=>{
         const newArr = [...todoList];
         newArr[index].isComplete = !newArr[index].isComplete;
         setTodoList(newArr)
+    };
     
-    }
+    const handleDeleteButtonClick = (id) => {setTodoList(prev => prev.filter((el)=>el.id !== id))}
     return(
         <>
             {todoList.map((todo, index)=>{return(
                 <div key={todo.id}>
                     <label>
+
                         <input
                         type="checkbox" 
                         onChange={()=>toggleCheckbox(index)}
                         checked={todo.isComplete}/>
+
                         {todo.todo}
+
                         <button >♻</button>
-                        <button onClick={()=>{setTodoList(prev => prev.filter((el)=>el.id !== todo.id))
+
+                        <button onClick={()=>{handleDeleteButtonClick(todo.id)
                         }}>❌</button>
                     </label>
                 </div>
@@ -28,5 +35,6 @@ function TodoList ({todoList, setTodoList}) {
         </>
     )
 }
+
 
 export default TodoList;
