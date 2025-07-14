6bb7f3b247420e1ae574d4e93609f09e8df8c81b
Yong-097 <vosejgo@gmail.com>
2025-07-14 16:40:35 +0900

    todoProject complete

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const filter = [
     {id: 0, label: "전체보기"},
     {id: 1, label: "완료된 항목 숨기기"},
     {id: 2, label: "완료된 항목만 보기"},
 ]
+
+function Filter ({filterId, setFilterId}) {
+    
+    return(
+        <>
+            <select
+            value={filterId} 
+            onChange={(e)=>{setFilterId(Number(e.target.value))}}>
+                {filter.map((el)=> 
+                <option
+                key={el.id}
+                value={el.id}>
+                    {el.label}
+                    </option>
+            )}
+            </select>
+        </>
+    )
+}
+
+export default Filter;
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,6 +2,7 @@ import TodoInput from "./TodoInput";
 import './Todo.css';
 import { useState } from "react";
 import TodoList from "./todoList";
+import Filter from "./Filter";
 
 
 function Todo () {
@@ -11,6 +12,16 @@ function Todo () {
             {id: 2, todo: '청소하기', isComplete: false},
         ])
     const [inputValue, setInputValue] = useState("")
+    const [filterId, setFilterId] = useState(0)
+
+    const getFilteredList = ()=>{
+        if(filterId === 1){
+            return todoList.filter((el)=> !el.isComplete)
+        }else if(filterId === 2){
+            return todoList.filter((el)=> el.isComplete)
+        }
+        return todoList;
+    }
     return(
         <>
             <div className="Todo">
@@ -22,8 +33,13 @@ function Todo () {
                     setInputValue={setInputValue}/>
                 </div>
                 <div>
+                    <Filter
+                    filterId={filterId}
+                    setFilterId={setFilterId}/>
+                </div>
+                <div>
                     <TodoList
-                    todoList={todoList}
+                    todoList={getFilteredList()}
                     setTodoList={setTodoList}/>
                 </div>
             </div>
